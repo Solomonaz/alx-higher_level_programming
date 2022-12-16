7eba11f597f869abaca8b059963bc9a26c45e105
Solomon <solomoab1921@gmail.com>
2022-12-16 01:12:21 -0800

    code status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,13 @@
 fetches https://intranet.hbtn.io/status.
 """
 
+
 if __name__ == '__main__':
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        response = res.read()
-    print("Body response:")
-    print("     - type: ", type(response))
-    print("     - content: ", response)
-    print("     - utf8 content: ", response.decode('utf-8'))
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        content = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
