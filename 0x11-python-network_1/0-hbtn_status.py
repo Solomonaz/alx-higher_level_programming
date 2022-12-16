eddaf2f76875a4b868f71edd6d8c7655a8f16f0f
Solomon <solomoab1921@gmail.com>
2022-12-16 01:10:19 -0800

    getting url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-    response = res.read()
-print("Body response:")
-print("     - type: ", type(response))
-print("     - content: ", response)
-print("     - utf8 content: ", response.decode('utf-8'))
+"""A script that
+fetches https://intranet.hbtn.io/status.
+"""
+
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        response = res.read()
+    print("Body response:")
+    print("     - type: ", type(response))
+    print("     - content: ", response)
+    print("     - utf8 content: ", response.decode('utf-8'))
